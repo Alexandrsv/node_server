23a76785454fb82ac470987612c918d685d24459
Александр <zbc@mail.ru>
2022-04-23 21:27:14 +0300

    experiments with middleware

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ import usersRouter from "./users/users.js";
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Время запроса: ", new Date().toLocaleString());
+  next();
+});
+
 app.all(/.*/, (req, res, next) => {
-  console.log("ALL HERE");
+  console.log("ALL REGEXP");
   next();
 });
 
 app.get("/hi", (req, res) => {
-  res.end();
+  throw new Error("Ошибка в запросе");
 });
 
 app.use("/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  console.log("Ошибка в запросе: ", { err });
+  res.status(500).send(`${err}`);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const userRouter = express.Router();
 
+userRouter.use((req, res, next) => {
+  console.log("Обработчик пользователей вызван");
+  next();
+});
+
 userRouter.post("/login", (req, res) => {
   res.send("login");
 });
