04c4dcf6f2522350e101d56dcb8ac077b0610fcc
Александр <zbc@mail.ru>
2022-04-23 21:06:36 +0300

    add userRouter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import usersRouter from "./users/users.js";
 
 const port = 8000;
 const app = express();
@@ -8,37 +9,11 @@ app.all(/.*/, (req, res, next) => {
   next();
 });
 
-app
-  .route("/hi")
-  .get((req, res) => {
-    res.cookie("token", "12345РАДОСТЬ!", {
-      path: "/",
-      // httpOnly: true,
-      secure: true,
-      // sameSite: "strict",
-    });
-    res.append("Bip", "bop bop bop");
-    res.type("application/json");
-    res.location("");
-    res.links({
-      next: "http://localhost:8000/hi",
-      last: "http://localhost:8000/hi",
-    });
-    res.status(202).json({ message: "Да что ты?", list: [1, 2, 3, 5, 6] });
-  })
-  .post((req, res) => {
-    res.set("Content-Type", "text/plain");
-    res.clearCookie("token");
-    res.send("Да что ты?");
-  });
-
-app.get("/download", (req, res) => {
-  res.download("./index.js", "src.js");
+app.get("/hi", (req, res) => {
+  res.end();
 });
 
-app.get("/redirect", (req, res) => {
-  res.redirect(301, "/hi");
-});
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
