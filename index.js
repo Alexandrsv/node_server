91bc27a1b69ba15e08826c8c2839fa93b4f6b440
Александр <zbc@mail.ru>
2022-04-23 20:14:51 +0300

    rewrite simple http server to expressjs

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
-import http from "http";
+import express from "express";
 
-const host = "127.0.0.1";
 const port = 8000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  switch (req.method) {
-    case "GET":
-      switch (req.url) {
-        case "/hello":
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.end("В чем разница между уткой?");
-          break;
-        case "/api/users":
-          res.end(
-            JSON.stringify([
-              {
-                id: 1,
-                name: "John Doe",
-                email: "",
-              },
-            ])
-          );
-          break;
-        default:
-          res.writeHead(404, { "Content-Type": "text/plain" });
-          res.end("Страница не найдена");
-      }
-      break;
-    case "POST":
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Неси это кольцо в мордовию!");
-      break;
-    default:
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Неа");
-  }
+app.get("/hello", (req, res) => {
+  res.send("Да что ты?");
 });
 
-server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
