2bf74ad219e31c09c474e23312f9d0d4c09b96c6
Александр <zbc@mail.ru>
2022-04-23 20:55:11 +0300

    some experiments

--- index.js
+++ index.js
@@ -8,24 +8,37 @@ app.all(/.*/, (req, res, next) => {
   next();
 });
 
-const cb1 = (req, res, next) => {
-  console.log("CB1");
-  next();
-};
-
-const cb2 = (req, res, next) => {
-  console.log("CB2");
-  next();
-};
-
 app
-  .route("/user")
+  .route("/hi")
   .get((req, res) => {
-    res.send("Да что ты?");
+    res.cookie("token", "12345РАДОСТЬ!", {
+      path: "/",
+      // httpOnly: true,
+      secure: true,
+      // sameSite: "strict",
+    });
+    res.append("Bip", "bop bop bop");
+    res.type("application/json");
+    res.location("");
+    res.links({
+      next: "http://localhost:8000/hi",
+      last: "http://localhost:8000/hi",
+    });
+    res.status(202).json({ message: "Да что ты?", list: [1, 2, 3, 5, 6] });
   })
   .post((req, res) => {
+    res.set("Content-Type", "text/plain");
+    res.clearCookie("token");
     res.send("Да что ты?");
   });
+
+app.get("/download", (req, res) => {
+  res.download("./index.js", "src.js");
+});
+
+app.get("/redirect", (req, res) => {
+  res.redirect(301, "/hi");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
