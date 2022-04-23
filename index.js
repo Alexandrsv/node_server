fb6536e94de209df9e4b7da77c14f61b763d8175
Александр <zbc@mail.ru>
2022-04-23 20:36:29 +0300

    add route

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ import express from "express";
 const port = 8000;
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Да что ты?");
+app.all(/.*/, (req, res, next) => {
+  console.log("ALL HERE");
+  next();
 });
+
+const cb1 = (req, res, next) => {
+  console.log("CB1");
+  next();
+};
+
+const cb2 = (req, res, next) => {
+  console.log("CB2");
+  next();
+};
+
+app
+  .route("/user")
+  .get((req, res) => {
+    res.send("Да что ты?");
+  })
+  .post((req, res) => {
+    res.send("Да что ты?");
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
