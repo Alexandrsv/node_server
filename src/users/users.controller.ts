267d588a0a5554414cf4efd1750845babe184e33
Александр <zbc@mail.ru>
2022-04-24 16:09:32 +0300

    add ExceptionFilter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
 import { NextFunction, Request, Response } from "express";
+import { HttpError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
   constructor(logger: LoggerService) {
@@ -12,10 +13,10 @@ export class UserController extends BaseController {
   }
 
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, { message: "login" });
+    next(new HttpError(401, "ошибка авторизации", "login"));
   }
 
   register(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, { message: "register" });
+    this.ok(res, { message: "пользователь создан" });
   }
 }
