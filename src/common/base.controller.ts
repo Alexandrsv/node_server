0651a889ba07d48ad54a6665e16cf791f45affde
Александр <zbc@mail.ru>
2022-04-24 13:47:02 +0300

    add abstract class BaseController

---
+++ src/common/base.controller.ts
@@ -0,0 +1,37 @@
+import { LoggerService } from "../logger/logger.service";
+import { Response, Router } from "express";
+import { IControllerRoute } from "./route.interface";
+
+export abstract class BaseController {
+  private readonly _router: Router;
+
+  constructor(private logger: LoggerService) {
+    this.logger = logger;
+  }
+
+  get router(): Router {
+    return this._router;
+  }
+
+  public created(res: Response): Response {
+    return res.sendStatus(201);
+  }
+
+  public send<T>(res: Response, code: number, data: T): Response {
+    return res.status(code).json(data);
+  }
+
+  public ok<T>(res: Response, data: T): Response {
+    return this.send<T>(res, 200, data);
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]): void {
+    for (const route of routes) {
+      this.logger.log(
+        `Binding route ${route.method.toUpperCase()} ${route.path}`
+      );
+      const handler = route.func.bind(this);
+      this.router[route.method](route.path, handler);
+    }
+  }
+}
