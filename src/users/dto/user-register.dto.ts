289907669a066cafc4a23be21c1fbe2326985d73
Александр <zbc@mail.ru>
2022-04-30 00:14:39 +0300

    add register validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsString } from "class-validator";
+
 export class UserRegisterDto {
+  @IsString({ message: "Username is not a string" })
   name: string;
+  @IsString({ message: "Password is not valid" })
   password: string;
+  @IsEmail({}, { message: "Email is not valid" })
   email: string;
 }
